8ea2a4f225f1692ddbc49d65c9067474636e17bd
mathiaskliem <kliem.mathias@gmail.com>
2023-03-03 13:46:59 +0100

    changement d'une erreur

--- config/config.class.php
+++ config/config.class.php
@@ -3,13 +3,12 @@
 abstract class config
 {
     // Definition des paramètres de la BDD
-    public static $DBHost = "localhost";
-
     public const DBNAME = "sae401";
+    public static $DBHost = "localhost";
 
     // Définition des paramètres du site
     public static $DBUser = "root";      // Titre affiché dans le header
 
     // Menu par défaut
-    public static $DBPwd = "roro";
+    public static $DBPwd = "MOTDEPASSE";
 }
