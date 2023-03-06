bf530d7f94c36592b17acb877cd19b60b1e67062
Mathias KLIEM <kliem.mathias@gmail.com>
2023-03-06 09:26:08 +0100

    Update config.class.php

--- config/config.class.php
+++ config/config.class.php
@@ -10,5 +10,5 @@ abstract class config
     public static $DBUser = "root";      // Titre affiché dans le header
 
     // Menu par défaut
-    public static $DBPwd = "roro";
+    public static $DBPwd = "MOTDEPASSE";
 }
