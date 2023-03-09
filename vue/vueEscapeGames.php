ff2cb74293a18ae43a226f57ff6068e466fd613e
vovArks Clavure <darkroiduchocapic@gmail.com>
2023-03-09 17:23:46 +0100

    Ajout page Escape Games
    - Ajout de la page listant tout les escapes games ;
    - Ajout des photos des membres de l'équipe de Kaisersthul restants.

---
+++ vue/vueEscapeGames.php
@@ -0,0 +1,47 @@
+<h2 class="titleUnderline">Escape Games</h2>
+
+<!-- Codex -->
+<div class="escapeGamesAlign">
+    <h2 class="titleYellow">The CODEX</h2>
+    <img class="imgBorder" src="../img/codex.jpg" alt="The Codex">
+    <p>Le Codex comprend le package complet.Ici, vous pouvez réserver les trois aventures de la série Codex à un prix spécial. Les dates des trois aventures sont encore ouvertes et vous pouvez choisir librement. L'ordre n'a pas d'importance.</p>
+    <a class="greenLink" href="index.php?action=escapeGame&escapeGame=codex">Lien vers la page de l'escape game</a>
+</div>
+<div class="contourTitleImage" style="padding-bottom: 50px; margin-bottom: 50px;"></div>
+
+<!-- In Vino Veritas -->
+<div class="escapeGamesAlign">
+    <h2 class="titleYellow">In Vino Veritas</h2>
+    <img class="imgBorder" src="../img/inVinoVeritas.jpg" alt="In Vinos Veritas">
+    <p>Le professeur Blankenberg est mondialement connu pour ses mérites dans le domaine de la viticulture. C'est uniquement grâce à ses années de recherche qu'en 1870, l'ensemble des vignes du Kaiserstuhl a été sauvé de l'attaque du phylloxéra [....]</p>
+    <a class="greenLink" href="index.php?action=escapeGame&escapeGame=vinoVeritas">Lien vers la page de l'escape game</a>
+</div>
+<div class="contourTitleImage" style="padding-bottom: 50px; margin-bottom: 50px;"></div>
+
+<!-- In Cantata Vin -->
+<div class="escapeGamesAlign">
+    <h2 class="titleYellow">In Cantata Vinum</h2>
+    <img class="imgBorder" src="../img/InCantataVinum.jpg" alt="In Cantata Vinum">
+    <p></p>
+    <a class="greenLink" href="index.php?action=escapeGame&escapeGame=inCantataVinum">Lien vers la page de l'escape game</a>
+</div>
+<div class="contourTitleImage" style="padding-bottom: 50px; margin-bottom: 50px;"></div>
+
+<!-- Buch der 7 Sigel -->
+<div class="escapeGamesAlign">
+    <h2 class="titleYellow">Buch der 7 Sigel</h2>
+    <img class="imgBorder" src="../img/buchDer7Siegel.jpg" alt="Buch der 7 Sigel">
+    <p></p>
+    <a class="greenLink" href="index.php?action=escapeGame&escapeGame=buchDer7Sigel">Lien vers la page de l'escape game</a>
+</div>
+<div class="contourTitleImage" style="padding-bottom: 50px; margin-bottom: 50px;"></div>
+
+<!-- Fluch der 9 Linden -->
+<div class="escapeGamesAlign">
+    <h2 class="titleYellow">Fluch der 9 Linden</h2>
+    <img class="imgBorder" src="../img/9Linden.jpg" alt="Fluch der 9 Linden">
+    <p></p>
+    <a class="greenLink" href="index.php?action=escapeGame&escapeGame=fluchDer9Linden">Lien vers la page de l'escape game</a>
+</div>
+<div class="contourTitleImage" style="padding-bottom: 50px; margin-bottom: 50px;"></div>
+
