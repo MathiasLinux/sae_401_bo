fc4cc47aac5e67537c3d15447e7687830111e4ca
Xem <xem.pro.contact@gmail.com>
2023-03-24 15:15:02 +0100

    add menu admin +page escapeGame admin

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,22 @@ let url = new URL(window.location.href);
 let action = url.searchParams.get("action");
 document.querySelector(".mobileMenuDiv").addEventListener("click", function () {
     if (action == "admin") {
-        document.querySelector(".mobileMenuAdmin").classList.add("show");
+       console.log('welcome');
     } else {
         document.querySelector(".mobileMenu").classList.add("show");
+    }
+});
+
+// if (action == "admin") {
+//     document.querySelector(".mobileMenuAdmin").classList.add("show");
+// } else {
+//     document.querySelector(".PCMenuDiv").classList.add("none");}
+
+document.querySelector(".PCMenuDiv").addEventListener("click", function () {
+    if (action == "admin") {
+        document.querySelector(".mobileMenuAdmin").classList.add("show");
+    } else {
+        document.querySelector(".PCMenuDiv").classList.add("none");
     }
 });
 
--- vue/vueAdminEscapeGames.php
+++ vue/vueAdminEscapeGames.php
@@ -1,23 +1,28 @@
 <h2 class="titleUnderline">Escape Games</h2>
 <div class="allEscapeGamesAdmin">
     <div class="anEscapeGameAdmin">
-        <img src="img/codex.jpg" alt="Escape game image">
+        <img src="img/escapeGames/1.jpg" alt="Escape game image">
         <p>The Codex</p>
         <a class="greenButton" href="index.php?action=admin&page=escapeGame&id=1">Modify</a>
     </div>
     <div class="anEscapeGameAdmin">
-        <img src="img/codex.jpg" alt="Escape game image">
-        <p>The Codex</p>
-        <a class="greenButton" href="index.php?action=admin&page=escapeGame&id=1">Modify</a>
+        <img src="img/escapeGames/2.jpg" alt="Escape game image">
+        <p>In Vino Vertitas</p>
+        <a class="greenButton" href="index.php?action=admin&page=escapeGame&id=2">Modify</a>
     </div>
     <div class="anEscapeGameAdmin">
-        <img src="img/codex.jpg" alt="Escape game image">
-        <p>The Codex</p>
-        <a class="greenButton" href="index.php?action=admin&page=escapeGame&id=1">Modify</a>
+        <img src="img/escapeGames/3.jpg" alt="Escape game image">
+        <p>In Cantata Vinium</p>
+        <a class="greenButton" href="index.php?action=admin&page=escapeGame&id=3">Modify</a>
     </div>
     <div class="anEscapeGameAdmin">
-        <img src="img/codex.jpg" alt="Escape game image">
-        <p>The Codex</p>
-        <a class="greenButton" href="index.php?action=admin&page=escapeGame&id=1">Modify</a>
+        <img src="img/escapeGames/4.jpg" alt="Escape game image">
+        <p>Buch der 7 Siegel</p>
+        <a class="greenButton" href="index.php?action=admin&page=escapeGame&id=4">Modify</a>
+    </div>
+    <div class="anEscapeGameAdmin">
+        <img src="img/escapeGames/5.jpg" alt="Escape game image">
+        <p>Fluch der 9 Linden</p>
+        <a class="greenButton" href="index.php?action=admin&page=escapeGame&id=5">Modify</a>
     </div>
 </div>
