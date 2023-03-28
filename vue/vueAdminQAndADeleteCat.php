388e09653d0855bab4b4a4bf235a694d3e572f14
Benvalda <107179283+Benvalda@users.noreply.github.com>
2023-03-28 13:37:31 +0200

    Version PC de Q&A Admin
    Si vous remarquer un problème, n'hésitez pas à m'en faire part

--- vue/vueAdminQAndADeleteCat.php
+++ vue/vueAdminQAndADeleteCat.php
@@ -1,16 +1,17 @@
 <a href="index.php?action=admin&page=qAndA"><button class="returnAdmin"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg> <?= ADMIN_QANDA_RETURN ?> </button></a>
 <div class="deuxTiers">
     <form class="formAdmin" action="index.php?action=admin&page=qAndADeleteCat_S&id_qAndACat=<?= $qAndAs['id_qAndACat'] ?>" method="post">
-    <label>
-        <p class="titleAdminQAndA">
+    <label class="center">
+        <h3 class="adminQandA titleAdminQAndA">
             <?php
-                echo ADMIN_QANDA_DELETE_CAT.": <br>";
+                echo ADMIN_QANDA_DELETE_CAT.": <div>";
                 if($_SESSION['lang'] == 'fr')
                     echo $qAndAs['titleFR'];
                 else
                     echo $qAndAs['title'];
+                echo "</div>";
             ?>
-        </p>
+        </h3>
     </label>
     <input class="redButtonQandA" type="submit" value=" <?= ADMIN_QANDA_DELETE_CAT_SUBMIT ?>">
     </form>
