49a80ed7015917d10d68ae1ad149c4283c25343d
Xem <xem.pro.contact@gmail.com>
2023-04-03 08:53:19 +0200

    Vue Jobs PC

--- vue/vueJobs.php
+++ vue/vueJobs.php
@@ -3,25 +3,23 @@
 ?>
 <h2 class="titleUnderline"><?= JOBS_TITLE ?></h2>
 <div class="jobsFirstIcons jobsIcons">
-    <div class="doubleLigneJobs">
-        <div class="jobsIcon">
-            <img src="img/svg/moneyHand.svg" alt="Money in a hand">
-            <p><?= JOBS_LOGO_1_1 ?></p>
-        </div>
-        <div class="jobsIcon">
-            <img src="img/svg/work.svg" alt="Briefcase">
-            <p><?= JOBS_LOGO_1_2 ?></p>
-        </div>
+    <div class="jobsIcon">
+        <img src="img/svg/moneyHand.svg" alt="Money in a hand">
+        <p><?= JOBS_LOGO_1_1 ?></p>
     </div>
-    <div class="doubleLigneJobs">
-        <div class="jobsIcon">
-            <img src="img/svg/audience.svg" alt="3 people">
-            <p><?= JOBS_LOGO_1_3 ?></p>
-        </div>
-        <div class="jobsIcon">
-            <img src="img/svg/presentation.svg" alt="Presentation with a group of people">
-            <p><?= JOBS_LOGO_1_4 ?></p>
-        </div>
+    <div class="jobsIcon">
+        <img src="img/svg/work.svg" alt="Briefcase">
+        <p><?= JOBS_LOGO_1_2 ?></p>
+    </div>
+
+
+    <div class="jobsIcon">
+        <img src="img/svg/audience.svg" alt="3 people">
+        <p><?= JOBS_LOGO_1_3 ?></p>
+    </div>
+    <div class="jobsIcon">
+        <img src="img/svg/presentation.svg" alt="Presentation with a group of people">
+        <p><?= JOBS_LOGO_1_4 ?></p>
     </div>
     <div class="singleLigneJobs">
         <div class="jobsIcon">
@@ -32,25 +30,21 @@
 </div>
 <h2 class="titleUnderline"><?= JOBS_H2_1 ?></h2>
 <div class="jobsFirstIcons jobsIcons">
-    <div class="doubleLigneJobs">
-        <div class="jobsIcon">
-            <img src="img/svg/userResponsability.svg" alt="User with responsabilties">
-            <p><?= JOBS_LOGO_2_1 ?></p>
-        </div>
-        <div class="jobsIcon">
-            <img src="img/svg/positive.svg" alt="A human head with a sun into it">
-            <p><?= JOBS_LOGO_2_2 ?></p>
-        </div>
+    <div class="jobsIcon">
+        <img src="img/svg/userResponsability.svg" alt="User with responsabilties">
+        <p><?= JOBS_LOGO_2_1 ?></p>
     </div>
-    <div class="doubleLigneJobs">
-        <div class="jobsIcon">
-            <img src="img/svg/team.svg" alt="A team of persons">
-            <p><?= JOBS_LOGO_2_3 ?></p>
-        </div>
-        <div class="jobsIcon">
-            <img src="img/svg/exchange.svg" alt="Two persons making a exchange">
-            <p><?= JOBS_LOGO_2_4 ?></p>
-        </div>
+    <div class="jobsIcon">
+        <img src="img/svg/positive.svg" alt="A human head with a sun into it">
+        <p><?= JOBS_LOGO_2_2 ?></p>
+    </div>
+    <div class="jobsIcon">
+        <img src="img/svg/team.svg" alt="A team of persons">
+        <p><?= JOBS_LOGO_2_3 ?></p>
+    </div>
+    <div class="jobsIcon">
+        <img src="img/svg/exchange.svg" alt="Two persons making a exchange">
+        <p><?= JOBS_LOGO_2_4 ?></p>
     </div>
 </div>
 <h2 class="titleUnderline"><?= JOBS_H2_2 ?></h2>
@@ -75,61 +69,67 @@
                 <h3><?= $jobTitle ?></h3>
                 <p><?= $jobPosition ?></p>
             </div>
-            <div class="taskJobOffer">
-                <p>Your Task :</p>
-                <ul>
-                    <?php
-                    if ($_SESSION['lang'] == "fr") {
-                        $jobTask = $job['taskFR'];
-                    } elseif ($_SESSION['lang'] == "en") {
-                        $jobTask = $job['task'];
-                    } else {
-                        $jobTask = $job['task'];
-                    }
-                    //Detection the \r et on le remplace par un <li>
-                    $jobTaskFinal = str_replace("\r", "<li>", $jobTask);
-                    //On ajoute le <li> au début de la chaine
-                    $jobTaskFinal = "<li>" . $jobTaskFinal;
-                    echo $jobTaskFinal . "</li>";
-                    ?>
-                </ul>
+            
+            <div class="jobFlexPC">
+                <div class="taskJobOffer">
+                    <p>Your Task :</p>
+                    <ul>
+                        <?php
+                        if ($_SESSION['lang'] == "fr") {
+                            $jobTask = $job['taskFR'];
+                        } elseif ($_SESSION['lang'] == "en") {
+                            $jobTask = $job['task'];
+                        } else {
+                            $jobTask = $job['task'];
+                        }
+                        //Detection the \r et on le remplace par un <li>
+                        $jobTaskFinal = str_replace("\r", "<li>", $jobTask);
+                        //On ajoute le <li> au début de la chaine
+                        $jobTaskFinal = "<li>" . $jobTaskFinal;
+                        echo $jobTaskFinal . "</li>";
+                        ?>
+                    </ul>
+                </div>
+                <div class="green wit"></div>
+                <div class="strenghtJobOffer">
+                    <p>Your strenght :</p>
+                    <ul>
+                        <?php
+                        if ($_SESSION['lang'] == "fr") {
+                            $jobStrength = $job['strengthFR'];
+                        } elseif ($_SESSION['lang'] == "en") {
+                            $jobStrength = $job['strength'];
+                        } else {
+                            $jobStrength = $job['strength'];
+                        }
+                        //Detection the \r et on le remplace par un <li>
+                        $jobStrengthFinal = str_replace("\r", "<li>", $jobStrength);
+                        //On ajoute le <li> au début de la chaine
+                        $jobStrengthFinal = "<li>" . $jobStrengthFinal;
+                        echo $jobStrengthFinal . "</li>";
+                        ?>
+                    </ul>
+                </div>
             </div>
-            <div class="strenghtJobOffer">
-                <p>Your strenght :</p>
-                <ul>
-                    <?php
-                    if ($_SESSION['lang'] == "fr") {
-                        $jobStrength = $job['strengthFR'];
-                    } elseif ($_SESSION['lang'] == "en") {
-                        $jobStrength = $job['strength'];
-                    } else {
-                        $jobStrength = $job['strength'];
-                    }
-                    //Detection the \r et on le remplace par un <li>
-                    $jobStrengthFinal = str_replace("\r", "<li>", $jobStrength);
-                    //On ajoute le <li> au début de la chaine
-                    $jobStrengthFinal = "<li>" . $jobStrengthFinal;
-                    echo $jobStrengthFinal . "</li>";
-                    ?>
-                </ul>
-            </div>
+            
         </div>
         <?php
     }
     ?>
 </div>
 <h2 class="titleUnderline"><?= JOBS_H2_3 ?></h2>
-<div class="contactInfos">
-    <div>
-        <img src="img/svg/tel.svg" alt="Phone Icon">
-        <a href="tel:017666810096">0176 66810096</a>
-    </div>
-    <div>
-        <img src="img/svg/email.svg" alt="Mail Icon">
-        <a href="mailto:booking@we-escape.de">booking@we-escape.de</a>
-    </div>
-</div>
+
 <form class="contactForm contactJobs" action="#" method="post">
+    <div class="contactInfos">
+        <div>
+            <img src="img/svg/tel.svg" alt="Phone Icon">
+            <a href="tel:017666810096">0176 66810096</a>
+        </div>
+        <div>
+            <img src="img/svg/email.svg" alt="Mail Icon">
+            <a href="mailto:booking@we-escape.de">booking@we-escape.de</a>
+        </div>
+    </div>
     <div class="formGroup">
         <label>
             <?= CONTACT_FORM_FIRST_NAME ?>
@@ -154,13 +154,13 @@
             <input type="tel" id="phone" name="phone">
         </label>
     </div>
-    <div class="formGroup">
+    <div class="formGroup nop">
         <label>
             <?= CONTACT_FORM_MESSAGE ?>
             <textarea name="message" id="message" cols="30" rows="10"></textarea>
         </label>
     </div>
-    <div class="formGroup formInput">
+    <div class="formGroup formInput right">
         <input type="submit" value="<?= CONTACT_FORM_SUBMIT ?>">
     </div>
 </form>
