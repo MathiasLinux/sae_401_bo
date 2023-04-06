962ce07a0b663d6f46e69a140a48b5df67366bd6
Xem <xem.pro.contact@gmail.com>
2023-04-06 09:33:23 +0200

    Page Escape Gamessss PC

--- vue/vueEscapeGames.php
+++ vue/vueEscapeGames.php
@@ -1,17 +1,16 @@
 <?php
-//var_dump($escapeGames);
-?>
-    <h2 class="titleUnderline">Escape Games</h2>
-
-<?php
+$counter = 0;
 foreach ($escapeGames as $escapeGame) {
+    $counter++;
     ?>
-    <div class="escapeGamesAlign">
-        <h2 class="titleYellow"><?php echo $escapeGame['name']; ?></h2>
-        <img class="imgBorder" src="img/escapeGames/<?php echo $escapeGame['id_escapeGame']; ?>.jpg"
+    <div class="escapeGamesAlign <?php echo ($counter % 2 == 0) ? 'escapeGamesReverse' : ''; ?>">
+        <h2 class="titleYellow titreEscape"><?php echo $escapeGame['name']; ?></h2>
+        <div class="imgEscape">
+            <img class="imgBorder " src="img/escapeGames/<?php echo $escapeGame['id_escapeGame']; ?>.jpg"
              alt="<?php echo $escapeGame['name']; ?>">
-        <p><?php echo $escapeGame['description']; ?></p>
-        <a class="greenLink" href="index.php?action=escapeGame&escapeGame=<?php echo $escapeGame['id_escapeGame']; ?>">Go the
+        </div>
+        <p class="paraEscape"><?php echo $escapeGame['description']; ?></p>
+        <a class="greenLink buttonEscape" href="index.php?action=escapeGame&escapeGame=<?php echo $escapeGame['id_escapeGame']; ?>">Go the
             escape game page</a>
     </div>
     <div class="contourTitleImage" style="padding-bottom: 50px; margin-bottom: 50px;"></div>
