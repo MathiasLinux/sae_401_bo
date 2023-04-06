1f6210f6e932285457cf56101b7e2c9e63cba2ae
Benvalda <107179283+Benvalda@users.noreply.github.com>
2023-04-06 17:29:00 +0200

    Update EscapeGames
    Ajout d'une image par défaut et gestion du statut d'une image

--- vue/vueEscapeGames.php
+++ vue/vueEscapeGames.php
@@ -6,8 +6,16 @@ foreach ($escapeGames as $escapeGame) {
     <div class="escapeGamesAlign <?php echo ($counter % 2 == 0) ? 'escapeGamesReverse' : ''; ?>">
         <h2 class="titleYellow titreEscape"><?php echo $escapeGame['name']; ?></h2>
         <div class="imgEscape">
-            <img class="imgBorder " src="img/escapeGames/<?php echo $escapeGame['id_escapeGame']; ?>.jpg"
-             alt="<?php echo $escapeGame['name']; ?>">
+            <?php 
+                if (file_exists("img/escapeGames/" . $escapeGame["id_escapeGame"] . ".jpg")) {
+                    $img = "img/escapeGames/" . $escapeGame["id_escapeGame"] . ".jpg";
+                } elseif (file_exists("img/escapeGames/" . $escapeGame["id_escapeGame"] . ".png")) {
+                    $img = "img/escapeGames/" . $escapeGame["id_escapeGame"] . ".png";
+                } else {
+                    $img = "img/escapeGames/default.jpg";
+                }
+            ?>
+            <img class="imgBorder " src="<?= $img ?>" alt="<?php echo $escapeGame['name']; ?>">
         </div>
         <p class="paraEscape"><?php echo $escapeGame['description']; ?></p>
         <a class="greenLink buttonEscape" href="index.php?action=escapeGame&escapeGame=<?php echo $escapeGame['id_escapeGame']; ?>">Go the
